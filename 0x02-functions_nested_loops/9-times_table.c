417c517f9a1778217989b134c5166e803088dedd
JOSHUA <omojolajoshua@gmail.com>
2022-09-13 22:06:55 +0000

    task 9 loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,16 +4,13 @@
  */
 void times_table(void)
 {
-	int k, n;
-        int count = 1;
+	int k, n, count = 1;
 
 	while (count <= 10)
 	{
 		if (count != 1)
-		{
 		_putchar(' ');
-		}
-		_putchar('0');
+	_putchar('0');
 		if (count != 10)
 		{
 		_putchar(',');
@@ -22,35 +19,28 @@ void times_table(void)
 		count++;
 	}
 		_putchar('\n');
-		for (n = 1; n < 10; n++)
-		{
-			for (k = 0; k < 10 * n; k += n)
+			for (n = 1; n < 10; n++)
 			{
-				if (k < 10)
-				{
+				for (k = 0; k < 10 * n; k += n)
+					if (k < 10)
+					{
 					if (k != 0)
-					{
-				_putchar(' ');
-					}
-				_putchar(k + '0');
+						_putchar(' ');
+					_putchar(k + '0');
 					if (k != 9 * n)
-					{
-					_putchar(',');
+						_putchar(',');
 					_putchar(' ');
 					}
-					}
-				else
-				{
-				_putchar(k / 10 + '0');
-				_putchar(k % 10 + '0');
-					if (k != 9 * n)
+					else
 					{
-					_putchar(',');
-					_putchar(' ');
+						_putchar(k / 10 + '0');
+					_putchar(k % 10 + '0');
+					if (k != 9 * n)
+						{
+						_putchar(',');
+						_putchar(' ');
+						}
 					}
-					}
-			}
-		_putchar('\n');
-		}
-
-	}
+				_putchar('\n');
+				}
+}
