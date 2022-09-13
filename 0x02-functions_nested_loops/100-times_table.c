acd3051c01145aeeb5e26af7852fb0442e3e1bd0
JOSHUA <omojolajoshua@gmail.com>
2022-09-13 23:26:03 +0000

    loops task 12a

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,23 +1,34 @@
 #include "main.h"
+/**
+ * print_times - prints multiplication table
+ * @n: the nth multiplication table to be printed
+ * Return: Always 0.
+ */
 void print_times_table(int n)
 {
 	int k;
 	int p;
-	int count =1;
-	if (n>15){
+	int count = 1;
+	if (n > 15)
+	{
 		return;
 	}
-	else if (n<0){
+	else if (n < 0)
+	{
 		return;
 	}
-	else{
+	else
+	{
 
-	while (count <= (n+1)){
-		if (count !=1)	{
+	while (count <= (n + 1))
+	{
+		if (count != 1)
+		{
 		_putchar(' ');
 		}
 		_putchar('0');
-		if (count!=(n+1)){
+		if (count != (n + 1))
+		{
 		_putchar(',');
 		_putchar(' ');
 		_putchar(' ');
@@ -26,41 +37,49 @@ void print_times_table(int n)
 		count++;
 	}
 		_putchar('\n');
-		for (p=1; p<(n+1); p++){
-			for (k=0; k<((n+1)*p); k+=p){	
-				if (k< 10){
-					if (k!=0){
+		for (p = 1; p < (n + 1); p++)
+		{
+			for (k = 0; k < ((n + 1) * p); k += p)
+			{
+				if (k < 10)
+				{
+					if (k != 0)
+					{
 				_putchar(' ');
 				_putchar(' ');
 					}
 				_putchar(k + '0');
-					if (k != n*p){
+					if (k != n * p)
+					{
 					_putchar(',');
 					_putchar(' ');
 					}
 				}
-				
-				else if(10 <= k && k < 100){
+				else if (10 <= k && k < 100)
+				{
 				_putchar(' ');
-				_putchar(k/10+'0');
-				_putchar(k%10+'0');
-					if (k != n*p){
+				_putchar(k / 10 + '0');
+				_putchar(k % 10 + '0');
+					if (k != n * p)
+					{
 					_putchar(',');
 					_putchar(' ');
-					}			
 					}
-				else {
-				_putchar(k/100+'0');	
-				_putchar((k-((k/100)*100))/10+'0');
-				_putchar(k%10+'0');
-					if (k != n*p){
+					}
+				else
+				{
+				_putchar(k / 100 + '0');
+				_putchar((k - ((k / 100) * 100)) / 10 + '0');
+				_putchar(k % 10 + '0');
+					if (k != n * p)
+					{
 					_putchar(',');
 					_putchar(' ');
 			}
 			}
 			}
 			_putchar('\n');
-		}	
+		}
 		return;
 	}
 }
