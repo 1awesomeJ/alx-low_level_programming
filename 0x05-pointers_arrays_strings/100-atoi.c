58ee9a44999f77258e3dc11a9b14acec08a84b52
JOSHUA <omojolajoshua@gmail.com>
2022-09-20 04:40:00 +0000

    advanced tasks attempt 3- C pointers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,12 +8,17 @@ int _atoi(char *s)
 {
 	int k = 0;
 	int i;
+	int sign = 1;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		k = k * 10 + s[i] - '0';
 	}
-	return (k);
+	if (s[0] == '-')
+	{
+		sign = -1;
+	}
+	return (k * sign);
 }
 
 
