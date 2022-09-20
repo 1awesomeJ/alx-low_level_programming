cda2c33a8b4af4f416c8b454a8c5b765443df872
JOSHUA <omojolajoshua@gmail.com>
2022-09-20 04:23:14 +0000

    advanced tasks attempt

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,19 @@
+/**
+ * _atoi - converts string to int
+ * @s: the string to be converted
+ * Return: Always 0.
+ */
+
+int _atoi(char *s)
+{
+	int k;
+	int i;
+
+	for (i = 0; i < 4; i++)
+	{
+		k = s[i];
+	}
+	return (k);
+}
+
+
