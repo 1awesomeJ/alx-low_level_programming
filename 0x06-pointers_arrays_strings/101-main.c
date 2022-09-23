fc0bcf535aa4e28c8d58aed49874697dac83056b
JOSHUA <omojolajoshua@gmail.com>
2022-09-23 08:25:11 +0000

    C more functions task 9

---
+++ 0x06-pointers_arrays_strings/101-main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_number(98);
+    _putchar('\n');
+    print_number(4000000);
+    _putchar('\n');
+    print_number(1020004);
+    _putchar('\n');
+    print_number(0);
+    _putchar('\n');
+    print_number(-98898999);
+    _putchar('\n');
+    return (0);
+}
