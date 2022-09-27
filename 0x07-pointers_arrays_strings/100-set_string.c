86d56fa4e3584caa31aa7d7509516a9498443d4d
JOSHUA <omojolajoshua@gmail.com>
2022-09-27 03:27:39 +0000

    C even more pointers set 5

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+/**
+ * set_string - sets the value of a pointer to a character
+ *@s: the pointer
+ *@to: the value to be set to
+ */
+
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * print_diagsums - prints the sum of the diagonal elements of 
+ * a square matrix of integers
+ * @size: the size of the square matrix
+ * @a: the square matrix.
+ */
+void print_diagsums(int (*a)[10], int size)
+{
+	int i, j, sum;
+	sum = 0;
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+		if (i == j)
+		{
+		sum += (a[i])[j];
+		}
+		}
+	}
+	printf("%d", sum);
+}
