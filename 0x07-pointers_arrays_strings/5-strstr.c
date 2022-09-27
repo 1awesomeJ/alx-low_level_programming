13f5bc356b3eb87bda5ad6520cacdfd6e8e6e607
JOSHUA <omojolajoshua@gmail.com>
2022-09-27 04:04:37 +0000

    C even more pointers task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -5,26 +5,31 @@
  * Return: pointer to the first occurrence of the substring.
  */
 
-char *_strstr(char *haystack, char *needle);
+char *_strstr(char *haystack, char *needle)
 
 {
-	int i ,j;
+	int i, j, k;
 
-	i= 0;
+	i = 0;
 	while (haystack[i] != '\0')
 	{
 		if (haystack[i] == needle[0])
 			break;
+		i++;
 	}
+		k = i;
 	for (j = 0; needle[j] != '\0'; j++)
 	{
-		if (haystack[i]==needle[j]
+		if (needle[j] == haystack[i])
+		{
+			;
+			i++;
+		}
+		else
+		{
+			return ('\0');
+		}
+	}
 
-	}
-		return (s + i);
-	}
-	else
-	{
-	return ('\0');
-	}
+	return (haystack + k);
 }
