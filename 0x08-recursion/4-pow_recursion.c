4213efb948ca71a63718b56499eda6f834e965b5
JOSHUA <omojolajoshua@gmail.com>
2022-09-28 20:55:27 +0000

    C recursions task 4a

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+/**
+ *_pow_recursion - calculates the value of an integer raised to a given 
+ * power
+ *@x: the integer which is raised to a power
+ *@y: the power/ exponent to which x is raised
+ *Return: the factorial of n.
+ */
+
+int _pow_recursion(int x, int y)
+
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, (y- 1)));
+}
+
