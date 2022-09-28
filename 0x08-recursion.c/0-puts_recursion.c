49d4383df937c2aaae8f51ac522f7c172cf069f8
JOSHUA <omojolajoshua@gmail.com>
2022-09-28 19:18:23 +0000

    C recursions task 0a

---
+++ 0x08-recursion.c/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string using just
+ * _putchar function without loops
+ *
+ *@s: the string to be printed( a pointer to it)
+ * Return: Always 0.
+ */
+
+
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+if (*s != '\0')
+	_puts_recursion(s + 1);
+if (*s == '\0')
+	_putchar('\n');
+}
+
---
+++ 0x08-recursion.c/README.md
@@ -0,0 +1,2 @@
+We started working with recursions in C.
+most of the functions we're writing are ones we had written in previous tasks uding loops, but now, we'll be writing them with recursions.
