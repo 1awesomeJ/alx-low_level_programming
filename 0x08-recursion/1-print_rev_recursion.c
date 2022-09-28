7245beedd296837b6e5f08139591a618e2a4646a
JOSHUA <omojolajoshua@gmail.com>
2022-09-28 20:05:50 +0000

    C recursions task 1a

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * using just _putchar function without loops
+ *
+ *@s: the string to be printed( a pointer to it)
+ * Return: Always 0.
+ */
+
+void _print_rev_recursion(char *s)
+{
+if (*(s + 1) != '\0')
+	_print_rev_recursion(s + 1);
+_putchar(*s);
+}
+
