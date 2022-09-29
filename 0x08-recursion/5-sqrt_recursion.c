99bfd0bd912a744933f95685282287005c15d2c3
JOSHUA <omojolajoshua@gmail.com>
2022-09-29 19:54:40 +0000

    C recursion task 5 b

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,13 +2,8 @@
  * check - checks for the square root of a number.
  *@n: number whose square root is to be found.
  *@c: initial guess of the square root value
- * _sqrt_reccursion - returns the square root of a number
  * Return: the square root value or -1 if number has no natural square root
  */
-
-
-
-
 int check(int n, int c)
 {
 	if (c * c == n)
@@ -18,7 +13,12 @@ int check(int n, int c)
 	return (check(n, c + 1));
 }
 
-
+/**
+  * _sqrt_recursion - returns the natural square root of a number
+  *@n: the number whose square root is to be found
+  * Return: the square root value
+  *or -1 if the number doesn't have a natural square root.
+  */
 int _sqrt_recursion(int n)
 
 {	int t;
