02a902ab411471c6ef8002f8cb4fec80d21375df
JOSHUA <omojolajoshua@gmail.com>
2022-09-29 20:15:09 +0000

    C recursion task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,25 +1,33 @@
 /**
- * is_prime_number - checks if an integer input is a prime number
- *@n: the integer to be checked
- * Return: 1 if n is prime, 0 if it's not.
+ * check - checks if a number is prime or not.
+ *@n: number to be checked.
+ *@c: divisor ro be used recursively on the number
+ * Return: 1 if the number is prime, and 0 if it isn't
  */
-
-int c = 2;
-int is_prime_number(int n)
+int check(int n, int c)
 {
-	if (n < 2)
-	{
-		return (0);
-	}
-	if (c >= n / 2 && n > 4)
-	{
-		return (1);
-	}
-	if (n % c == 0)
-	{
-		return (0);
-	}
-	c++;
-	return (is_prime_number(n));
-
+	if (n % c  == 0)
+	return (0);
+	if (c > n / 2 && n > 4)
+	return (1);
+	return (check(n, c + 1));
 }
+
+/**
+  *is_prime_number - tells whether or not a number is prime.
+  *@n: the number to be checked.
+  * Return: 0 for non prime numbers,
+  * 1 for prime numbers.
+  */
+
+int is_prime_number(int n)
+{	int t;
+	int c = 2;
+
+	if (n < 2)
+	t = 0;
+	else
+	t = check(n, c);
+	return (t);
+}
+
