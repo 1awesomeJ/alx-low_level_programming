b0590ed0cb00ae919c9ecfd0eb79567659a8cb5e
JOSHUA <omojolajoshua@gmail.com>
2022-09-30 21:23:55 +0000

    C argc, argv task 1a

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - makes a program print out the number of arguments passed to
+ * it during executin.
+ *@argc: counts the number of arguments passed during execution
+ *@argv: vector that stores all arguments as strings which they are
+ *Return:  0 on success
+ */
+int main(int argc, __attribute__((unused))char **argv)
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
