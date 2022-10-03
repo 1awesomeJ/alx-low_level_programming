ca47136fbc07492e347b16db639f5554e3de7dcc
JOSHUA <omojolajoshua@gmail.com>
2022-10-03 10:02:20 +0000

    C malloc, free task 0a

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+/**
+ * create_array - creates a specified size of contigous memory
+ * space dynamically,
+ * allocates it to a character array, and initializes it to a given character.
+ *@size: the size of the charcter array
+ *@c: the character with which the array
+ * Return: Always 0.
+ */
+
+
+
+
+char *create_array(unsigned int size, char c)
+
+{
+	char *set;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+	set = malloc(size * sizeof(char));
+
+	for (i = 0; i < size; i++)
+	{
+		set[i] = c;
+	}
+	return (set);
+
+}
+
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,3 @@
+We started working with Dynamic memory allocation using the malloc and free functions in C.
+this is a step up from the automatic memory allocation that the compiler has been doing for us up to now.
+It's not as though we're moving away from auto allocation, I'm not sure that'd be possible. We're just adding the capability of allocating memory dynamically to our codes
