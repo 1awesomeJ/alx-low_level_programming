6360bcd55875cbcd944ef9092ed8c56b051f55c4
JOSHUA <omojolajoshua@gmail.com>
2022-10-06 12:41:07 +0000

    C more malloc, free task 1a

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <string.h>
+/**
+ * string_nconcat - adds the first n bytes of a second string,to a first string
+ *@s1: the first string
+ *@s2: the second string
+ *@n: the number of bytes to be copied from s2
+ * Return: a pointer to the address of the concantenated string
+ */
+
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int a, c, i, j;
+	char *b;
+/* first, we should get the length of s1 to know how much space to allocate*/
+
+	if (s1 == NULL)
+		a = 0;
+	if (s2 == NULL)
+		c = 0;
+	if (s1 != NULL)
+	a = strlen(s1);
+	if (s2 != NULL)
+	c = strlen(s2);
+	if (n < c)
+	{
+	b = malloc(a + n + 1);
+	if (b == NULL)
+		return (NULL);
+	}
+	if (n >= c)
+	{
+	b = malloc(a + c + 1);
+	if (b == NULL)
+		return (NULL);
+	}
+/*now we copy s1 first into the allocted space, s1 will occupy index 0 to a-1*/
+
+	for (i = 0; i < a; i++)
+		b[i] = s1[i];
+
+/*Finally we copy the first n bytes of s2 to the rem space, i=0 4 s2 from 0*/
+
+	i = 0;
+	for (j = a; j < (a + n); j++)
+	{
+		b[j] = s2[i];
+		i++;
+	}
+
+	return (b);
+}
