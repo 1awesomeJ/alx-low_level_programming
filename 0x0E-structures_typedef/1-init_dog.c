acec71c3700e20c15d19e324fe469c9f309275d2
JOSHUA <omojolajoshua@gmail.com>
2022-10-11 11:53:40 +0000

    C structs corrections 1d

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 /**
  * init_dog - sets the values of data in the dog struct using pointers
  *@d: the address of the struct
@@ -12,8 +13,11 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 
 {
+	if (d != NULL)
+	{
 	(*d).name = name;
 	(*d).age = age;
 	(*d).owner = owner;
+	}
 
 }
