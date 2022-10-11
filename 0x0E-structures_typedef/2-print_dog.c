6c10a54cad982edf5757509d27b5844889e49e74
JOSHUA <omojolajoshua@gmail.com>
2022-10-11 12:45:07 +0000

    C structs corrections 2b

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,20 +12,20 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 {
-	if ((*d).name == NULL)
+	if ((*d).name == NULL && (*d).owner != NULL)
 {
 	printf("Name: (nil)\n");
 	printf("Age: %f\n", (*d).age);
 	printf("Owner: %s\n", (*d).owner);
 }
 
-	else if ((*d).age == NAN)
+	else if ((*d).name == NULL && (*d).owner == NULL)
 {
-	printf("Name: %s\n", (*d).name);
-	printf("Age: (nil)");
-	printf("Owner: %s\n", (*d).owner);
+	printf("Name: (nil)\n");
+	printf("Age: %f\n", (*d).age);
+	printf("Owner: (nil)\n");
 }
-	else if ((*d).owner == NULL)
+	else if ((*d).owner == NULL && (*d).name != NULL)
 {
 	printf("Name: %s\n", (*d).name);
 	printf("Age: %f\n", (*d).age);
