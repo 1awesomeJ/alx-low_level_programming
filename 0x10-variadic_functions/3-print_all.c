e28a69a4cf9b67b163d0beee2688a6dd1d5355a5
JOSHUA <omojolajoshua@gmail.com>
2022-10-15 14:09:50 +0000

    C corrections 3 b

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,6 +1,5 @@
 #include <stdarg.h>
 #include <stdio.h>
-#include <string.h>
 /**
  * print_all - prints any type of variable number of arguments.
  *
@@ -10,44 +9,36 @@
 
 
 void print_all(const char * const format, ...)
-{	int i, j, k;
-	char *p, *str;
+{	int i;
+	char *str;
 	va_list prnt;
+	char *sep;
 
-	k = strlen(format);
-	p = "cfis";
 	va_start(prnt, format);
 	i = 0;
-	while (i < k)
-	{	j = 0;
-	while (j < k)
+	sep = "";
+	while (format && format[i])
 	{
-	if (format[i] == p[j])
+	switch (format[i])
 	{
-	switch (j)
-	{ case 0:
-		printf("%c", va_arg(prnt, int));
+		case 'c':
+		printf("%s%c", sep, va_arg(prnt, int));
 		break;
-		case 1:
-		printf("%f", va_arg(prnt, double));
+		case 'f':
+		printf("%s%f", sep, va_arg(prnt, double));
 		break;
-		case 2:
-		printf("%d", va_arg(prnt, int));
+		case 'i':
+		printf("%s%d", sep, va_arg(prnt, int));
 		break;
-		case 3:
+		case 's':
 		str = va_arg(prnt, char*);
 		if (str == NULL)
 			str = "(nil)";
-		printf("%s", str);
+		printf("%s%s", sep, str);
 		break;
-		default:
-		continue;
 	}
-	}
-		if (i != (k - 1) &&  format[i] == p[j])
-		printf(", ");
-		j++; }
-	i++;
+		sep = ", ";
+		i++;
 	}
 	va_end(prnt);
 	printf("\n");
