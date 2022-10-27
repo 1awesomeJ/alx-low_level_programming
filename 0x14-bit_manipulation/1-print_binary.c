11a5e9861c6e6f6709e2a28aa57bf93a6b613262
JOSHUA <omojolajoshua@gmail.com>
2022-10-27 22:01:53 +0000

    C bit manipulation task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,2 +1,29 @@
+#include "main.h"
+/**
+ * print_binary - prints the binary value of an unsigned integer
+ *using bit manipulation and recursion.
+ *@n: the unsigned long integer to be printed.
+ */
+
+
+
 void print_binary(unsigned long int n)
 {
+	if (n == 0)
+	{
+	_putchar('0');
+	return;
+	}
+	if (n == 1)
+	{
+	_putchar ('1');
+	return;
+	}
+	if (n > 1)
+	print_binary(n >> 1);
+
+	if (n & 1)
+	_putchar ('1');
+	else
+	_putchar ('0');
+}
