3adbfec96e5495b5cd9c589718ba18cdef4af1ed
JOSHUA <omojolajoshua@gmail.com>
2022-10-28 04:55:58 +0000

    C bit manipulation task 5 attempt

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,15 @@
+/**
+ * flip_bits - returns the number of digits that must be flipped
+ * in one number to get a second number
+ *@n: the number whose digits are to be flipped
+ *@m: the number to be gotten
+ * Return: the number of digits to be flipped.
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	if (n && m)
+	return (2);
+
+	return (0);
+}
