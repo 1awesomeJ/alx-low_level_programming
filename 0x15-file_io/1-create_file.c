98f7228fe093a68e278d89e4206a5f374f0d3302
JOSHUA <omojolajoshua@gmail.com>
2022-10-31 23:47:07 +0000

    file i/o 1c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,8 +24,10 @@ int create_file(const char *filename, char *text_content)
 
 	if (!(text_content))
 		return (1);
-	size_w = write(fileDes, text_content, k + 1);
+	size_w = write(fileDes, text_content, k);
+
 	if (size_w < 0)
 		return (-1);
+
 	return (1);
 }
