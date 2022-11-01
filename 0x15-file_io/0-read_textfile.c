1047cbaeb59ab05ba80e398364e9fcb50f22ecdd
JOSHUA <omojolajoshua@gmail.com>
2022-11-01 09:21:33 +0000

    C file i-o task 0 corrections

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * read_textfile - reads from a file and outputs it to the console
+ *@filename: name of file to be read
+ *@letters: number of characters to be read.
+ * Return: Always 0.
+ */
+
+
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buffer;
+	ssize_t size_r, size_w;
+
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(letters * sizeof(char));
+
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+	return (0);
+
+	size_r = read(fd, buffer, letters);
+	if (size_r < 0)
+	return (0);
+
+	size_w = write(1, buffer, letters);
+	if (size_w < 0 || size_w < (ssize_t) letters)
+		return (0);
+
+	return (size_r);
+}
+
+
+
--- 0x15-file_io/README.md
+++ 0x15-file_io/README.md
@@ -1,1 +1,3 @@
 We started working on File i/O in C.
+We are working with the open syscall with is various options: O_WRONLY, O_RDONLY, O_RDWR, O_CREAT, O_TRUNC, O_APPEND 
+we're also using file descriptors gotten from the open function to carry out READ and WRITE on files.
