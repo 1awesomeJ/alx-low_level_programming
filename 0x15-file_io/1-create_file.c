65e261c685106c1191d4fd766d87990c48d59858
JOSHUA <omojolajoshua@gmail.com>
2022-11-01 08:42:39 +0000

    C file i/o corrections 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,6 @@ int create_file(const char *filename, char *text_content)
 	ssize_t size_w;
 	size_t k;
 
-	k = strlen(text_content);
 
 	if (!(filename))
 		return (-1);
@@ -24,6 +23,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (!(text_content))
 		return (1);
+
+	k = strlen(text_content);
 	size_w = write(fileDes, text_content, k);
 
 	if (size_w < 0)
