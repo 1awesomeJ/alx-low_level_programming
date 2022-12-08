ce39bf7f50e1af68d3f8f9231f0d48af7d0be2db
JOSH <omojolajoshua@gmail.com>
2022-12-08 22:34:53 +0000

    fixed betty task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ * print_dlistint - prints the elements of a dlistint list
+ *@h: head pointer of the list.
+ * Return: Always EXIT_SUCCESS.
+ */
+
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t k = 0;
+
 	while (h)
 	{
 	printf("%d\n", h->n);
 	k++;
 	h = h->next;
 	}
-	return k;
+	return (k);
 }
