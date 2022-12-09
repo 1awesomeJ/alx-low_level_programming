f5cd41b8ed1fd126a02035cedd1a4338da989082
JOSH <omojolajoshua@gmail.com>
2022-12-09 00:36:22 +0000

    task 5

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,28 @@
+#include <stddef.h>
+#include "lists.h"
+
+
+/**
+ * get_dnodeint_at_index - gets the element at a given index of a linked list
+ *@head: the head pointer of the list
+ *@index: the required index
+ * Return: Always EXIT_SUCCESS.
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i = 1;
+
+	while (head)
+	{
+		head = head->next;
+		i++;
+		if (i == index + 1)
+		break;
+	}
+	if (i < index)
+		return (NULL);
+
+	return (head);
+}
+
