ad70e61ebcb236b8d99712c069721666ad243d19
JOSH <omojolajoshua@gmail.com>
2022-12-09 00:53:00 +0000

    task 6

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,23 @@
+#include <stddef.h>
+#include "lists.h"
+/**
+ * sum_dlistint - returns the sum of the lements of a doubly-linked list
+ *@head: the head pointer of the list.
+ * Return: Always EXIT_SUCCESS.
+ */
+
+
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+	return (0);
+while (head)
+i	{
+	sum += head->n;
+	head = head->next;
+	}
+return (sum);
+}
