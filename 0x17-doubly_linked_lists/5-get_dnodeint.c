409b80d1d4de580e512f178e0a765cadd54d983f
JOSH <omojolajoshua@gmail.com>
2022-12-09 00:42:54 +0000

    taskkk 5

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -20,7 +20,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 		if (i == index + 1)
 		break;
 	}
-	if (i < index + 2)
+	if (i < index + 1)
 		return (NULL);
 
 	return (head);
